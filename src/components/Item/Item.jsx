b158d68c17539d3e8291c1971d3f075ef9f8b1c4
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-09-04 20:17:59 -0300

    Ajuste ItemDetail

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -9,9 +9,6 @@ export const Item = ({item}) => {
       <Card.Body>
         <Card.Title className='card-name text-center'>{item.name}</Card.Title>
         <Card.Text className='card-price text-center'>${item.price}</Card.Text>
-        {/* <Link to={`/item/${item.id}`} className="card-btn-container">
-          <Button variant="primary" className='card-btn mx-auto d-block'>Ver más</Button>
-        </Link> */}
         <Link to={`/item/${item.id}`} className="card-btn btn btn-dark d-block">
           Ver más
         </Link>
