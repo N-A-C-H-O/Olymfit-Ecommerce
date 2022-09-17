6d98215e604246fdc515448073889d6b6560254d
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-09-17 10:31:32 -0300

    Implementación addDoc Firestore

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -4,12 +4,22 @@ import { MdError } from "react-icons/md";
 import { useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../utils/Firebase";
+import { useEffect } from "react";
 
 export const Checkout = () => {
-    const [name,setName] = useState("");
-    const [surname,setSurname] = useState("");
-    const [email,setEmail] = useState("");
-    const [tel,setTel] = useState("");
+    const {cartList,getTotalPrice} = useContext(CartContext);
+
+    const [inputName,setInputName] = useState("");
+
+    const [inputSurname,setInputSurname] = useState("");
+    
+    const [inputEmail,setInputEmail] = useState("");
+
+    const [inputTel,setInputTel] = useState("");
 
     const [validForm, setValidForm] = useState({
         name: false,
@@ -18,24 +28,26 @@ export const Checkout = () => {
         tel: false
     });
 
+    const [orderId,setOrderId] = useState("");
+
     const validName = () => {
         const regex = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-        regex.test(name) ? setValidForm({ ...validForm, name: true }) : setValidForm({ ...validForm, name: false });
+        regex.test(inputName) ? setValidForm({ ...validForm, name: true }) : setValidForm({ ...validForm, name: false });
     }
 
     const validSurname = () => {
         const regex = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-        regex.test(surname) ? setValidForm({ ...validForm, surname: true }) : setValidForm({ ...validForm, surname: false });
+        regex.test(inputSurname) ? setValidForm({ ...validForm, surname: true }) : setValidForm({ ...validForm, surname: false });
     }
 
     const validEmail = () => {
         const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-        regex.test(email) ? setValidForm({ ...validForm, email: true }) : setValidForm({ ...validForm, email: false });
+        regex.test(inputEmail) ? setValidForm({ ...validForm, email: true }) : setValidForm({ ...validForm, email: false });
     }
 
     const validTel = () => {
         const regex = /^[0-9]+$/;
-        regex.test(tel) ? setValidForm({ ...validForm, tel: true }) : setValidForm({ ...validForm, tel: false });
+        regex.test(inputTel) ? setValidForm({ ...validForm, tel: true }) : setValidForm({ ...validForm, tel: false });
     }
 
     const errorNotif = (message) => {
@@ -65,11 +77,30 @@ export const Checkout = () => {
     const sendOrder = (e) => {
         e.preventDefault();
         if (validForm.name && validForm.surname && validForm.email && validForm.tel) {
+            const order = {
+                buyer: {
+                    name: inputName,
+                    surname: inputSurname,
+                    email: inputEmail,
+                    tel: inputTel
+                },
+                items: cartList,
+                total: getTotalPrice(),
+            }
+            const ordersCollection = collection(db,"orders");
+            addDoc(ordersCollection,order).then(({id}) => setOrderId(id)).catch((err) => {
+                console.log(err);
+                errorNotif('Error! Inténtelo más tarde');
+            });
             successNotif('Datos enviados correctamente!')
         } else {
             errorNotif('Sus datos son incorrectos. Por favor, rellene el formulario.')
         }
     }
+
+    useEffect(() => {
+        console.log(orderId)
+    },[orderId])
 
     
     return(
@@ -77,30 +108,30 @@ export const Checkout = () => {
             <form onSubmit={sendOrder}>
                 <div className="form-section">
                     <label>Nombre</label>
-                    <input onKeyUp={validName} onChange={(e) => setName(e.target.value)} type="text" placeholder="Nombre"/>
+                    <input onKeyUp={validName} onChange={(e) => setInputName(e.target.value)} type="text" placeholder="Nombre"/>
                     {
-                        validForm.name ? <span><AiFillCheckCircle/></span> : <span><MdError/></span>
+                        validForm.name ? <span><AiFillCheckCircle/></span> : inputName === "" ? "" : <span><MdError/></span>
                     } 
                 </div>
                 <div className="form-section">
                     <label>Apellido</label>
-                    <input onKeyUp={validSurname} onChange={(e) => setSurname(e.target.value)} type="text" placeholder="Apellido"/>
+                    <input onKeyUp={validSurname} onChange={(e) => setInputSurname(e.target.value)} type="text" placeholder="Apellido"/>
                     {
-                        validForm.surname ? <span><AiFillCheckCircle/></span> : <span><MdError/></span>
+                        validForm.surname ? <span><AiFillCheckCircle/></span> : inputSurname === "" ? "" : <span><MdError/></span>
                     } 
                 </div>
                 <div className="form-section">
                     <label>Email</label>
-                    <input onKeyUp={validEmail} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Correo electrónico"/>
+                    <input onKeyUp={validEmail} onChange={(e) => setInputEmail(e.target.value)} type="text" placeholder="Correo electrónico"/>
                     {
-                        validForm.email ? <span><AiFillCheckCircle/></span> : <span><MdError/></span>
+                        validForm.email ? <span><AiFillCheckCircle/></span> : inputEmail === "" ? "" : <span><MdError/></span>
                     } 
                 </div>
                 <div className="form-section">
                     <label>Teléfono</label>
-                    <input onKeyUp={validTel} onChange={(e) => setTel(e.target.value)} type="text" placeholder="Número de teléfono" maxLength={15}/>
+                    <input onKeyUp={validTel} onChange={(e) => setInputTel(e.target.value)} type="text" placeholder="Número de teléfono" maxLength={15}/>
                     {
-                        validForm.tel ? <span><AiFillCheckCircle/></span> : <span><MdError/></span>
+                        validForm.tel ? <span><AiFillCheckCircle/></span> : inputTel === "" ? "" : <span><MdError/></span>
                     } 
                 </div>
                 <button className="btn d-block mt-5">Finalizar compra</button>
